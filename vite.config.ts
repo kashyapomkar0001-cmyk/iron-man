c62e70608026c76b504a85d41371d32bb6aa5826
kashyapomkar0001-cmyk <kashyapomkar0001@gmail.com>
2026-08-06 00:10:35 +0530

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    host: true,
+    port: 5173,
+  },
+
+  preview: {
+    host: true,
+    port: 4173,
+  },
+
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    target: "esnext",
+  },
+
+  optimizeDeps: {
+    include: [
+      "three",
+      "react",
+      "react-dom",
+      "@mediapipe/tasks-vision"
+    ]
+  }
+});
